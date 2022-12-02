f51f32833341b0237641be1f902754fc866e3cf3
Jordi Amoros <jfamoros80@gmail.com>
2022-12-02 17:33:18 +0100

    minor changes

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,48 +1,34 @@
-use std::collections::HashMap;
-#[macro_use]
-extern crate lazy_static;
-
-lazy_static! {
-    static ref SCORES1: HashMap<&'static str, u32> = {
-        let mut scores = HashMap::new();
-        scores.insert("A X", 4);
-        scores.insert("A Y", 8);
-        scores.insert("A Z", 3);
-        scores.insert("B X", 1);
-        scores.insert("B Y", 5);
-        scores.insert("B Z", 9);
-        scores.insert("C X", 7);
-        scores.insert("C Y", 2);
-        scores.insert("C Z", 6);
-        scores
-    };
-
-    static ref SCORES2: HashMap<&'static str, u32> = {
-        let mut scores = HashMap::new();
-        scores.insert("A X", 3);
-        scores.insert("A Y", 4);
-        scores.insert("A Z", 8);
-        scores.insert("B X", 1);
-        scores.insert("B Y", 5);
-        scores.insert("B Z", 9);
-        scores.insert("C X", 2);
-        scores.insert("C Y", 6);
-        scores.insert("C Z", 7);
-        scores
-    };
-}
-
 pub fn part_one(input: &str) -> Option<u32> {
     Some(input.split("\n")
-        .map(|x| SCORES1.get(&x).unwrap())
-        .sum()
+        .fold(0, |acc, x| acc + match x {
+            "A X" => 4,
+            "A Y" => 8,
+            "A Z" => 3,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 7,
+            "C Y" => 2,
+            "C Z" => 6,
+            _ => 0
+        })
     )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     Some(input.split("\n")
-        .map(|x| SCORES2.get(&x).unwrap())
-        .sum()
+        .fold(0, |acc, x| acc + match x {
+            "A X" => 3,
+            "A Y" => 4,
+            "A Z" => 8,
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+            "C X" => 2,
+            "C Y" => 6,
+            "C Z" => 7,
+            _ => 0
+        })
     )
 }
 
